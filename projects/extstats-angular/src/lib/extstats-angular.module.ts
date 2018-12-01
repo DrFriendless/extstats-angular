b020b76b18b5cdf84d5be527126b7ef19eee591f
DrFriendless <friendless.farrell@gmail.com>
2018-12-01 22:18:26 +1100

    v1.1.0 added geek-list-editor

--- projects/extstats-angular/src/lib/extstats-angular.module.ts
+++ projects/extstats-angular/src/lib/extstats-angular.module.ts
@@ -7,12 +7,24 @@ import {ButtonGroupComponent} from "./button-group/button-group.component";
 import {ButtonGroupButtonDirective} from "./button-group-button.directive";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { NguiAutoCompleteModule } from '@ngui/auto-complete';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {GeekChipsComponent} from "./geek-chips/geek-chips.component";
+import {GeekComboComponent} from "./geek-combo/geek-combo.component";
+import {GeekListEditorComponent} from "./geek-list-editor/geek-list-editor.component";
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatInputModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
-    NgbModule.forRoot(), NguiAutoCompleteModule, FormsModule, BrowserModule
+    NgbModule.forRoot(), NguiAutoCompleteModule, FormsModule, BrowserModule,
+    ReactiveFormsModule, MatFormFieldModule, MatAutocompleteModule, HttpClientModule, MatInputModule,
+    BrowserAnimationsModule, MatChipsModule, MatIconModule
   ],
   declarations: [
     ChartDirective,
@@ -20,7 +32,10 @@ import { BrowserModule } from '@angular/platform-browser';
     DocumentationComponent,
     TableConfigComponent,
     ButtonGroupComponent,
-    ButtonGroupButtonDirective
+    ButtonGroupButtonDirective,
+    GeekChipsComponent,
+    GeekComboComponent,
+    GeekListEditorComponent
   ],
   exports: [
     ChartDirective,
@@ -28,7 +43,10 @@ import { BrowserModule } from '@angular/platform-browser';
     DocumentationComponent,
     TableConfigComponent,
     ButtonGroupComponent,
-    ButtonGroupButtonDirective
+    ButtonGroupButtonDirective,
+    GeekChipsComponent,
+    GeekComboComponent,
+    GeekListEditorComponent
   ]
 })
 export class ExtstatsAngularModule { }
