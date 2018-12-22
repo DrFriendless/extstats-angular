8e9dac4bfd28b54127ef14aaceeb255301c5d270
DrFriendless <friendless.farrell@gmail.com>
2018-12-23 10:25:54 +1100

    v3.0 move vega out into extstats-vega

--- projects/extstats-angular/src/lib/extstats-angular.module.ts
+++ projects/extstats-angular/src/lib/extstats-angular.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
-import {ChartDirective} from "./chart.directive";
-import {ChartPaneComponent} from "./chart-pane/chart-pane.component";
 import {DocumentationComponent} from "./extstats-documentation/documentation.component";
 import {TableConfigComponent} from "./table-config/table-config.component";
 import {ButtonGroupComponent} from "./button-group/button-group.component";
 import {ButtonGroupButtonDirective} from "./button-group-button.directive";
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { NguiAutoCompleteModule } from '@ngui/auto-complete';
 import { BrowserModule } from '@angular/platform-browser';
 import {GeekChipsComponent} from "./geek-chips/geek-chips.component";
@@ -19,16 +16,16 @@ import { MatInputModule } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   imports: [
-    NgbModule.forRoot(), NguiAutoCompleteModule, FormsModule, BrowserModule,
+    NguiAutoCompleteModule, FormsModule, BrowserModule,
     ReactiveFormsModule, MatFormFieldModule, MatAutocompleteModule, HttpClientModule, MatInputModule,
-    BrowserAnimationsModule, MatChipsModule, MatIconModule
+    BrowserAnimationsModule, MatChipsModule, MatIconModule, TabsModule.forRoot(), CollapseModule.forRoot()
   ],
   declarations: [
-    ChartDirective,
-    ChartPaneComponent,
     DocumentationComponent,
     TableConfigComponent,
     ButtonGroupComponent,
@@ -38,8 +35,6 @@ import { MatIconModule } from '@angular/material/icon';
     GeekListEditorComponent
   ],
   exports: [
-    ChartDirective,
-    ChartPaneComponent,
     DocumentationComponent,
     TableConfigComponent,
     ButtonGroupComponent,
--- projects/extstats-angular/src/public_api.ts
+++ projects/extstats-angular/src/public_api.ts
@@ -3,7 +3,6 @@
  */
 
 export * from './lib/extstats-angular.module';
-export * from './lib/charts';
 export * from "./lib/button-group/button-group-interfaces";
 export * from "./lib/extstats-documentation/documentation";
 export * from "./lib/table-config/extstats-table";
