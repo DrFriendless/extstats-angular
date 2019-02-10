09f02cd6639fb0ef0bd2cfb268f6baf0bc0b919c
DrFriendless <friendless.farrell@gmail.com>
2019-02-10 16:33:28 +1100

    v3.3.2 added PlaysViewComponent and PlaysSourceComponent

--- projects/extstats-angular/src/public_api.ts
+++ projects/extstats-angular/src/public_api.ts
@@ -8,3 +8,5 @@ export * from "./lib/extstats-documentation/documentation";
 export * from "./lib/table-config/extstats-table";
 export * from './lib/data-source-component';
 export * from './lib/data-view-component';
+export * from './lib/plays-source-component';
+export * from './lib/plays-view-component';
