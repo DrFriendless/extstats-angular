a58bd33b9e3fce9c4973b334fc6951ca5126c215
DrFriendless <friendless.farrell@gmail.com>
2019-03-31 14:44:05 +1100

    v3.6.4 remove angular router dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'extstats-angular-app';
+  public foundGeek: string;
+
+  public choose(geek: string) {
+    this.foundGeek = geek;
+  }
 }
