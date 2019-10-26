33ad57b946d79039fbce0eb5abe0b280b6a95df3
DrFriendless <Borod1n>
2019-10-26 11:31:54 +1100

    v5.1.0 added GraphQuerySourceComponent

--- projects/extstats-angular/src/public_api.ts
+++ projects/extstats-angular/src/public_api.ts
@@ -3,12 +3,13 @@
  */
 
 export * from './lib/extstats-angular.module';
-export * from "./lib/button-group/button-group-interfaces";
-export * from "./lib/extstats-documentation/documentation";
+export * from './lib/button-group/button-group-interfaces';
+export * from './lib/extstats-documentation/documentation';
 export * from './lib/data-source-component';
 export * from './lib/config/config.component';
 export * from './lib/data-view-component';
 export * from './lib/plays-source-component';
 export * from './lib/plays-view-component';
+export * from './lib/graph-query-source.component';
 export * from './lib/loader/loader.component';
 export * from './lib/user-data.service';
